859f83aac39015ccf7644926c146ed0ffe036969
StanislavNikov <stanislavmnikov@gmail.com>
2021-12-17 12:25:13 +0100

    setting up for changePattern / switching to previousCoords{X, Y}

--- js/script.js
+++ js/script.js
@@ -8,30 +8,26 @@
         ctx = canvas.getContext('2d');
 
     let currentColor = changeColor.value,
-
         currentLineWidth = changeLineWidth.value,
         currentPattern = changePattern.value,
         isDrawing = false,
-        previousX = null,
-        previousY = null;
+        previousCoords = { x: null, y: null };
 
     captureCanvasDimensions();
 
-    // Function initiates previousX & previousY
-    function handleStart(e) {
+    // Function sets isDrawing to true
+    function handleStart() {
         isDrawing = true;
-        let x = e.pageX;
-        let y = e.pageY;
-        previousX = x;
-        previousY = y;
     }
 
-    // Function to stop drawing
+    // Function to stop drawing and reseting previousCoords
     function handleEnd() {
         isDrawing = false;
+        previousCoords.x = null;
+        previousCoords.y = null;
     }
 
-    // Function checks the value of isDrawing() and uses the previously initiated coordinates previousX, previousY to draw a line to x, y
+    // Function checks the value of isDrawing() then sets x,y to match the cursor coordinates and calls draw(x, y)
     function handleMove(e) {
         // To prevent drawing on hover
         if (!isDrawing) {
@@ -41,9 +37,19 @@
         let x = e.pageX;
         let y = e.pageY;
 
+        draw(x, y);
+    }
+
+    // Function to draw a line from previousCoords to x, y
+    function draw(x, y) {
+        if (!previousCoords.x) {
+            previousCoords.x = x;
+            previousCoords.y = y;
+        }
+
         // Draw a line from previousX/previousY to x/y
         ctx.beginPath();
-        ctx.moveTo(previousX, previousY);
+        ctx.moveTo(previousCoords.x, previousCoords.y);
         ctx.lineTo(x, y);
 
         // Set the style of the line
@@ -51,16 +57,14 @@
         ctx.strokeStyle = currentColor;
         ctx.stroke();
 
-        // Set previous coordinates for next move event
-        previousX = x;
-        previousY = y;
+        previousCoords.x = x;
+        previousCoords.y = y;
     }
 
     // Function captures the Dimensions of the Canvas
     function captureCanvasDimensions() {
         canvas.width = canvas.offsetWidth;
         canvas.height = canvas.offsetHeight;
-        clearCanvas();
     }
 
     // Function clears a rectangle starting from x=0, y=0 with width canvas.with and height canvas.height
@@ -79,6 +83,7 @@
     function handleChangePattern() {
         currentPattern = changePattern.value;
     }
+
     changeColor.addEventListener('change', handleChangeColor);
     changeLineWidth.addEventListener('change', handleChangeLineWidth);
     changePattern.addEventListener('change', handleChangePattern);
