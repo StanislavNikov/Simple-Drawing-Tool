816d85153cd405f8f815508e14feb2a7d57b2350
StanislavNikov <stanislavmnikov@gmail.com>
2021-12-17 11:10:55 +0100

    handleStart, handleEnd, handleMove f-s added, IIFE added

--- js/script.js
+++ js/script.js
@@ -1,21 +1,72 @@
-const changePattern = document.querySelector('#changePattern'),
-    changeLineWidth = document.querySelector('#changeLineWidth'),
-    changeColor = document.querySelector('#changeColor'),
-    canvas = document.querySelector('#canvas'),
-    ctx = canvas.getContext('2d');
+// IIFE
+(function() {
+    const changePattern = document.querySelector('#changePattern'),
+        changeLineWidth = document.querySelector('#changeLineWidth'),
+        changeColor = document.querySelector('#changeColor'),
+        canvas = document.querySelector('#canvas'),
+        ctx = canvas.getContext('2d');
 
-captureCanvasDimensions();
+    let isDrawing = false,
+        previousX = null,
+        previousY = null;
 
-// Function captures the Dimensions of the Canvas
-function captureCanvasDimensions() {
-    canvas.width = canvas.offsetWidth;
-    canvas.height = canvas.offsetHeight;
-    clearCanvas();
-}
+    captureCanvasDimensions();
 
-// Function clears a rectangle starting from x=0, y=0 with width canvas.with and height canvas.height
-function clearCanvas() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-}
+    // Function initiates previousX & previousY
+    function handleStart(e) {
+        isDrawing = true;
+        let x = e.pageX;
+        let y = e.pageY;
+        previousX = x;
+        previousY = y;
+    }
 
-window.addEventListener('resize', captureCanvasDimensions);
+    // Function to stop drawing
+    function handleEnd() {
+        isDrawing = false;
+    }
+
+    // Function checks the value of isDrawing() and uses the previously initiated coordinates previousX, previousY to draw a line to x, y
+    function handleMove(e) {
+        // To prevent drawing on hover
+        if (!isDrawing) {
+            return;
+        }
+
+        let x = e.pageX;
+        let y = e.pageY;
+
+        // Draw a line from previousX/previousY to x/y
+        ctx.beginPath();
+        ctx.moveTo(previousX, previousY);
+        ctx.lineTo(x, y);
+
+        // Set the style of the line
+        ctx.lineWidth = changeLineWidth.value;
+        ctx.strokeStyle = changeColor.value;
+        ctx.stroke();
+
+        // Set previous coordinates for next move event
+        previousX = x;
+        previousY = y;
+    }
+
+    // Function captures the Dimensions of the Canvas
+    function captureCanvasDimensions() {
+        canvas.width = canvas.offsetWidth;
+        canvas.height = canvas.offsetHeight;
+        clearCanvas();
+    }
+
+    // Function clears a rectangle starting from x=0, y=0 with width canvas.with and height canvas.height
+    function clearCanvas() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+
+    document.querySelector('#clearCanvas').addEventListener('click', clearCanvas);
+    canvas.addEventListener('pointerdown', handleStart);
+    canvas.addEventListener('pointerup', handleEnd);
+    canvas.addEventListener('pointercancel', handleEnd);
+    canvas.addEventListener('pointermove', handleMove);
+    window.addEventListener('resize', captureCanvasDimensions);
+})();
